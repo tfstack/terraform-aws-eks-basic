c91cb62a5c482a87785cc55e636b5d8fed9aadb7
John Ajera <37360952+jajera@users.noreply.github.com>
2026-04-24 08:28:16 +1200

    docs: update nodepool names
    better naming scheme

--- examples/auto-mode/karpenter-nodepool.tf
+++
@@ -1,5 +0,0 @@
-resource "kubernetes_manifest" "karpenter_accelerated_nodepool" {
-  manifest = yamldecode(file("${path.module}/manifests/karpenter-nodepool-accelerated.yaml"))
-
-  depends_on = [module.eks]
-}
